e0e3eb37c71d6d3e321b26a9b5e58dc9f90ac51c
laazyLlama <laazy.llama@gmail.com>
2021-04-26 18:40:38 +0200

    basic functionality complete

--- hangman.py
+++ hangman.py
@@ -1,1 +1,20 @@
-print("Hello World!")
+#------------------------------------------------------------------------------
+#Project Name:     hangman
+#Description:      A simple version of hangman coded in Python
+#------------------------------------------------------------------------------
+#Author:           laazyLlama
+#Date Created:     26/04/2021
+#Last Changes:     26/04/2021
+#------------------------------------------------------------------------------
+
+word = "Test"
+known = "...."
+
+while known.find('.') != -1:
+    print(known)
+    guess = input("Have a guess: ").lower()
+    for i in word:
+        if i == guess or i == guess.upper():
+            known = known[:(word.find(i))] + word[word.find(i)] + known[(word.find(i) + 1):]
+
+print(known + " was the hidden word!")
